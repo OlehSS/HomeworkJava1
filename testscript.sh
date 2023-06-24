6f75fa2183a20d44cd4e44cfe33ef2c0928c70a4
OlehSS <133905314+OlehSS@users.noreply.github.com>
2023-06-24 09:48:57 +0200

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,14 @@
  echo "You name is: $(whoami)"
  echo "You working directory is: $(pwd)"
  echo "done"
+
+// Дальнейших строк не было в коде:
+ Проверка работоспособности:
+  chmod 744 testscript.sh
+  ./testsctipt.sh
+ Убрать права на чтение:
+  chmod 344 testscript.sh
+ Убрать права на исполнение:
+  chmod 244 testscript.sh
+ Сделать файл скрытым:
+  mv /root/testscript.sh /root/.testscript.sh
