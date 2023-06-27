a36e2a9d061974f7485890519c367be21df61799
OlehSS <133905314+OlehSS@users.noreply.github.com>
2023-06-27 12:17:25 +0200

    Update HomeWork5.java

--- HomeWork5.java
+++ HomeWork5.java
@@ -1,23 +1,17 @@
 package Hw.Homeworks;
 
-import java.util.Scanner;
-
 public class HomeWork5 {
     public static void main(String[] args) {
 
         double PI = 3.14;
 
-        Scanner k = new Scanner(System.in);
 
         int y = 24;
-        double S = PI * (y / 2);
+        double S = PI * Math.pow((y / 2), 2);
         double l = S * 40;
-        // Теперь "l" будет для нашего задания константой
 
-        System.out.println("Enter pls diameter of your pizza: ");
-        int p = k.nextInt();
-
-        double S1 = PI * (p / 2);
+        int p = 28;
+        double S1 = PI * Math.pow((p / 2), 2);
         double l2 = S1 * 40;
 
         double result = l2 - l;
@@ -26,9 +20,8 @@ public class HomeWork5 {
         int roundedResult = (int) Math.floor(result);
 
 
-        System.out.println("You pizza have " + roundedResult + " calories than normal" );
+        System.out.println("Pizza 28sm have " + roundedResult + " calories more than 24sm" );
 
-        k.close();
 
     }
 }
