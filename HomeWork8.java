8186767e36ee07e2dead8f2e32fc45e4ecb5cd3e
OlehSS <133905314+OlehSS@users.noreply.github.com>
2023-07-04 12:53:13 +0200

    Add files via upload

---
+++ HomeWork8.java
@@ -0,0 +1,11 @@
+package Hw.Homeworks;
+
+public class HomeWork8 {
+    public static void main(String[] args) {
+        int [] x = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i] + " ");
+        }
+    }
+}
